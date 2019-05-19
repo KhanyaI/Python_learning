62d40f8d4c51be0741c73580c1b7d94074b323fb
KhanyaI <44304733+KhanyaI@users.noreply.github.com>
2019-05-19 17:28:29 +0200

    Update heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,17 +1,36 @@
-def buildheap
+
+def maxheap(A,heapify_size,i):
+	l = 2*i+1
+	r = 2*i+2
+	largest = i
+	if l < heapify_size and A[l] > A[largest]:
+		largest= l
+	if r < heapify_size and A[r] > A[largest]:
+		largest = r
+	if largest != i:
+		A[i],A[largest] = A[largest],A[i]
+		maxheap(A,heapify_size,largest)
 
 
-def maxheap(A,i):
-	l = A[i]-1
-	r = A[i]+1
-
+def buildheap(A,build_size):
+	for i in range (build_size//2,-1,-1):
+		print(i)
+		maxheap(A,build_size,i)
 
 
 
 def heapsort(A):
-	buildheap(A)
-	for i in range(len(A),2,-1):
-		A[1],A[i] = A[i],A[1]
-		#reduce heap size
-		maxheap(A,1)
-#trying
+	n = len(A)
+
+	buildheap(A,n)
+	while n > 0:
+		A[0],A[n-1] = A[n-1],A[0]
+		n = n-1
+		maxheap(A,n,0)
+	return A
+
+
+array = [ 2, 11, 13, 5, 4, 7] 
+heapsort(array)
+print(array)
+
