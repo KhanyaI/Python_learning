4517f9e4c54a6531f75644f4ec5ad55d841c101e
Ifrah <ifrah.94@gmail.com>
2019-08-14 00:44:46 +0200

    hw3 part 2

--- binary_multiplication.py
+++ binary_multiplication.py
@@ -2,9 +2,6 @@ import time
 import random
 
 def binarymulti(a,b):
-    #array1 = [int(x) for x in str(a)] #Taking the binary number as a string, then breaking it down into individual digits and return them as integers in an array
-    #array2 = [int(x) for x in str(b)]
-
     temp1 = [] # initialize an empty list outside the for loop
 
     for i in range(0,len(a)): #for every digit in the array, doesn't matter which one
@@ -12,19 +9,18 @@ def binarymulti(a,b):
         for j in range(0,len(b)): #for every
             product= a[i] * b[j]
             temp2.append(product)
-            #print (array1[i], array2[j], product)
         temp1.append(temp2)
-    #print(temp1)
+        #print(temp2)
+    print(temp1)
 
     sum = 0
     allproducts = []
 
     for i in range(0,len(a)):
-        j = [0] * i
+        j = [0] * (len(a)-(i+1))
         final = temp1[i]+j
-        #print(final)
-        num = int("".join(map(str, final)))
-        #print(num)
+        # add modulus
+        print(final)
         sum = sum + num
 
     print('The product of these two binary numbers is:', sum)
@@ -42,9 +38,13 @@ def flip(n):
 
 
 if __name__ == '__main__':
-    number_of_bits = 4
-    bit_list_1 = flip(number_of_bits)
-    print(bit_list_1)
+    len_bit_lst = [2,4,8,10]
+    for i in len_bit_lst[1:2]:
+
+    	bit_list_1 = flip(i)
+    	bit_list_2 = flip(i)
+    	print(bit_list_1,bit_list_2)
+    	binarymulti(bit_list_1,bit_list_2)
 
 
 
